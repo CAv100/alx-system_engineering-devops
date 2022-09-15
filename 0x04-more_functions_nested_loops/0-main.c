e7cd2ce9f16b8314496f7c290766166978f27ff9
CAv100 <101235161+CAv100@users.noreply.github.com>
2022-09-15 22:28:15 +0100

    Update 0-main.c

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -11,8 +11,8 @@ int main(void)
     char c;
 
     c = 'A';
-    print("%c: %d\n", c, _isupper(c));
+    printf("%c: %d\n", c, _isupper(c));
     c = 'a';
-    print("%c: %d\n", c, _isupper(c));
+    printf("%c: %d\n", c, _isupper(c));
     return (0);
 }
